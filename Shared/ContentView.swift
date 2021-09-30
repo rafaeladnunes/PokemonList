96e1b032918d0af6686af926e9006e4cdab7a16b
rafaela-nunes <90198825+rafaela-nunes@users.noreply.github.com>
2021-09-30 08:18:45 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Rafaela Nunes on 30/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PokemonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListApp.swift
+//  Shared
+//
+//  Created by Rafaela Nunes on 30/09/21.
+//
+
+import SwiftUI
+
+@main
+struct PokemonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
